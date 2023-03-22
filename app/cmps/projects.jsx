e19bad13745c70852ad71e33db4ecd989745ab40
Alon Mlievski <alonmlievski@gmail.com>
2023-03-22 15:21:31 +0200

    additional projects added

--- app/cmps/projects.jsx
+++ app/cmps/projects.jsx
@@ -29,7 +29,30 @@ export function Projects() {
       image: "tba",
       stack: ["Javascript", "CSS"],
     },
+    {
+      name: "Web Portfolio",
+      description: "This website",
+      url: "https://superduperalon.github.io/Minesweeper/",
+      image: "tba",
+      stack: ["React", "Next.js", "Sass"],
+    },
   ];
+
+  const otherProjects = [
+    {
+      name: "Simon",
+      description: "The classic game - Angela Yu Udemy Course",
+      url: "https://superduperalon.github.io/Meme-Generator/",
+      stack: ["Javascript", "CSS"],
+    },
+    {
+      name: "Movie Seats",
+      description: "Brad Traversy Udemy Course",
+      url: "https://superduperalon.github.io/Minesweeper/",
+      stack: ["Javascript", "CSS"],
+    },
+  ];
+
   return (
     <section className="intro-layout">
       <div>
@@ -45,6 +68,24 @@ export function Projects() {
                   </div>
                   <div>{project.description}</div>
                   <div>img</div>
+                  {project.stack.map((tech) => {
+                    return <li key={tech.idx}>{tech}</li>;
+                  })}
+                </section>
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          {otherProjects.map((project) => {
+            return (
+              <li key={project.idx}>
+                <section>
+                  <div>Featured Project</div>
+                  <div>
+                    <a href={`${project.url}`}>{project.name}</a>
+                  </div>
+                  <div>{project.description}</div>
                   {project.stack.map((tech) => {
                     return <li key={tech.idx}>{tech}</li>;
                   })}
