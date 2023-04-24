5209dd6a2a4a2c72ea37c204fb9c8af3278477cf
Alon Mlievski <alonmlievski@gmail.com>
2023-04-24 17:28:38 +0300

    testing window

--- app/page.jsx
+++ app/page.jsx
@@ -38,6 +38,12 @@ export default function Home() {
           {/* <Footer /> */}
         </section>
       )}
+
+      {isMobile && (
+        <section className="index-layout scroll">
+          <Intro />
+        </section>
+      )}
     </>
   );
 }
