009ef01ee5d5b19e1cb1e7898c07d1d07fd6f4cb
Alon Mlievski <alonmlievski@gmail.com>
2023-04-30 10:36:41 +0300

    io testing

--- app/hooks/useGlobalIntersectionObserver.jsx
+++ app/hooks/useGlobalIntersectionObserver.jsx
@@ -1,30 +1,30 @@
-const observedElements = [];
+// const observedElements = [];
 
-const handleIntersection = (entry) => {
-  console.log("Intersection observed: ", entry);
-};
+// const handleIntersection = (entry) => {
+//   console.log("Intersection observed: ", entry);
+// };
 
-const handleObserve = (element) => {
-  observedElements.push(element);
-  observer.observe(element);
-};
+// const handleObserve = (element) => {
+//   observedElements.push(element);
+//   observer.observe(element);
+// };
 
-const handleUnobserve = (element) => {
-  const index = observedElements.indexOf(element);
-  if (index !== -1) {
-    observedElements.splice(index, 1);
-  }
-  observer.unobserve(element);
-};
+// const handleUnobserve = (element) => {
+//   const index = observedElements.indexOf(element);
+//   if (index !== -1) {
+//     observedElements.splice(index, 1);
+//   }
+//   observer.unobserve(element);
+// };
 
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.66,
-};
+// const options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.66,
+// };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(handleIntersection);
-}, options);
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(handleIntersection);
+// }, options);
 
-export { observedElements, handleObserve, handleUnobserve, options };
+// export { observedElements, handleObserve, handleUnobserve, options };
