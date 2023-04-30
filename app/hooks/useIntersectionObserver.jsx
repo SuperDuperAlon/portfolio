7ef2fe6d25c1bf9e7180c2c025c5cb40e2adcdf0
Alon Mlievski <alonmlievski@gmail.com>
2023-04-30 10:33:10 +0300

    intersecion observer testing

--- app/hooks/useIntersectionObserver.jsx
+++ app/hooks/useIntersectionObserver.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
-import { observer } from "../utils/intersection-observer";
+// import { useEffect } from "react";
+// import { observer } from "../utils/intersection-observer";
 
-export function useIntersectionObserver() {
-  useEffect(() => {
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
+// export function useIntersectionObserver() {
+//   useEffect(() => {
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, []);
 
-  return function observe(className) {
-    const elements = document.querySelectorAll(`.${className}`);
-    console.log(elements);
-    elements.forEach((element) => {
-      observer.observe(element);
-    });
-  };
-}
+//   return function observe(className) {
+//     const elements = document.querySelectorAll(`.${className}`);
+//     console.log(elements);
+//     elements.forEach((element) => {
+//       observer.observe(element);
+//     });
+//   };
+// }
--- app/utils/intersection-observer.js
+++ app/utils/intersection-observer.js
@@ -1,16 +1,16 @@
-function handleIntersection(entry) {
-  console.log("Intersection observed: ", entry);
-  if (entry.isIntersecting) {
-    entry.target.classList.add("observed");
-  } 
-}
+// function handleIntersection(entry) {
+//   console.log("Intersection observed: ", entry);
+//   if (entry.isIntersecting) {
+//     entry.target.classList.add("observed");
+//   } 
+// }
 
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.1,
-};
+// const options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.1,
+// };
 
-export const observer = new IntersectionObserver((entries) => {
-  entries.forEach(handleIntersection);
-}, options);
+// export const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(handleIntersection);
+// }, options);
