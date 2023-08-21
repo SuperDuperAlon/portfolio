351868b9fa5993b6edc2fdf19c56c4cb374af892
Alon Mlievski <alonmlievski@gmail.com>
2023-08-21 23:52:47 +0300

    stack list

--- app/cmps/about.jsx
+++ app/cmps/about.jsx
@@ -25,24 +25,37 @@ export function About() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  const icons = [
-    { name: "Javascript (ES6+)", img: <SiJavascript /> },
-    { name: "CSS", img: <SiCss3 /> },
+  const languages = [
     { name: "HTML", img: <SiHtml5 /> },
+    { name: "Javascript (ES6+)", img: <SiJavascript /> },
     { name: "Typescript", img: <SiTypescript /> },
+    { name: "Java", img: <FaJava /> },
+  ]
+
+  const frameworks = [
     { name: "React", img: <FaReact /> },
-    { name: "Redux", img: <SiRedux /> },
     { name: "Next.js", img: <SiNextdotjs /> },
     { name: "Vue", img: <SiVuedotjs /> },
     { name: "Angular", img: <SiAngular /> },
+    { name: "Redux", img: <SiRedux /> },
+    { name: "Zustand", img: <SiRedux /> },
+  ]
+
+  const styling = [
+    { name: "CSS", img: <SiCss3 /> },
     { name: "Sass", img: <SiSass /> },
-    { name: "Node.js", img: <SiNodedotjs /> },
-    { name: "Express.js", img: <SiExpress /> },
-    { name: "Java", img: <FaJava /> },
-    { name: "SQL", img: <SiSqlite /> },
-    { name: "MongoDB", img: <SiMongodb /> },
     { name: "Bootstrap", img: <SiBootstrap /> },
     { name: "JQuery", img: <SiJquery /> },
+    { name: "Material UI", img: <SiJquery /> },
+    { name: "Headless UI", img: <SiJquery /> },
+  ]
+
+  const back = [
+    { name: "Node.js", img: <SiNodedotjs /> },
+    { name: "Express.js", img: <SiExpress /> },
+    { name: "SQL", img: <SiSqlite /> },
+    { name: "MongoDB", img: <SiMongodb /> },
+
   ];
 
   return (
@@ -65,23 +78,74 @@ export function About() {
             />
           </div>
           <p>
-A marketing professional turned Full Stack Developer who&apos;s <span className="highlight">passionate</span> about using code to create beautiful and seamless digital solutions. As a Coding Academy graduate, my primary stack is MERN, although these days I&apos;m incorporating more skills into my digital tool belt.
+            A marketing professional turned Full Stack Developer who&apos;s <span className="highlight">passionate</span> about using code to create beautiful and seamless digital solutions. As a Coding Academy graduate, my primary stack is MERN, although these days I&apos;m incorporating more skills into my digital tool belt.
           </p>
           <p>
-          I come with varied experience and soft skills from my past endeavors where I was a <span className="highlight">significant part</span> of the companies I&apos;ve worked at, helping them grow their revenue and clientele. 
+            I come with varied experience and soft skills from my past endeavors where I was a <span className="highlight">significant part</span> of the companies I&apos;ve worked at, helping them grow their revenue and clientele.
           </p>
           <p>
-          An ardent and <span className="highlight">organized autodidact</span>, who&apos;s truly passionate about learning and growing as a client-side developer.
+            An ardent and <span className="highlight">organized autodidact</span>, who&apos;s truly passionate about learning and growing as a client-side developer.
           </p>
-          <div className="stack-list mar-b16">
-            {icons.map((icon, idx) => {
-              return (
-                <div className="tech-stack-list" key={idx}>
-                  {icon.img}
-                  <div>{icon.name}</div>
-                </div>
-              );
-            })}
+
+          <div className="test">
+            <h1>Languages</h1>
+            <div className="stack-list mar-b16">
+              {languages.map((icon, idx) => {
+                return (
+                  <div className="tech-stack-list" key={idx}>
+                    <div>
+                    {icon.img}
+                    </div>
+                    <div>{icon.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="test">
+            <h1>Frameworks</h1>
+            <div className="stack-list mar-b16">
+              {frameworks.map((icon, idx) => {
+                return (
+                  <div className="tech-stack-list" key={idx}>
+                    <div>
+                    {icon.img}
+                    </div>
+                    <div>{icon.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="test">
+            <h1>Styling</h1>
+            <div className="stack-list mar-b16">
+              {styling.map((icon, idx) => {
+                return (
+                  <div className="tech-stack-list" key={idx}>
+                    <div>
+                    {icon.img}
+                    </div>
+                    <div>{icon.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="test">
+            <h1>Backend</h1>
+            <div className="stack-list mar-b16">
+              {back.map((icon, idx) => {
+                return (
+                  <div className="tech-stack-list" key={idx}>
+                    <div>
+                    {icon.img}
+                    </div>
+                    <div>{icon.name}</div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
